6b9ce5debc5723a97db0db0875402ac32a15c4a0
Makus14 <mesha.maksim14@gmail.com>
2024-12-24 16:15:07 +0300

    connect to mogo, but not tested

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetupDetails() {
   );
 }
 
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://tripwishlist.com/wp-content/uploads/2023/09/AdobeStock_152886744-scaled.webp",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some address 5, 12345 Some City",
+        description: "This is a first meetup!",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
